8eb33f91f5084bf95e0fdec0f959ed393601e763
jordan.dalby <jordan.dalby1@outlook.com>
2024-07-29 20:18:15 +0100

    Logging

--- src/presenter.py
+++ src/presenter.py
@@ -166,6 +166,9 @@ class DashCamPresenter:
                 while self.model.is_streaming:
                     buffer = self.model.picam2.capture_buffer("lores")
 
+                    self.logger.debug(f"Captured frame: {buffer}")
+                    self.logger.debug(f"Captured frame shape: {buffer.shape}")
+                    
                     rgb = cv2.cvtColor(buffer, cv2.COLOR_YUV420p2RGB)
 
                     ret, jpeg = cv2.imencode('.jpg', rgb)
